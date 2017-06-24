50676e948afc6164279cdfc97b592ed0f7e85971
Guillaume Grillon Labelle <guillaume.grillon.labelle@gmail.com>
2017-06-24 00:54:30 -0400

    import the BasicMenu package

---
+++ S6_LaserDefender/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public void LoadLevel(string name)
+	{
+		Debug.Log("New Level load: " + name);
+		SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest()
+	{
+		Debug.Log("Quit requested");
+#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+		 //Application.OpenURL(webplayerQuitURL);
+#else
+		 Application.Quit();
+#endif
+	}
+
+	public void LoadNextLevel()
+	{
+		StartCoroutine(LoadNextLevelScript());
+	}
+
+	private IEnumerator LoadNextLevelScript()
+	{
+		yield return new WaitForSecondsRealtime(.75f);
+
+		var nextLevelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+		if (nextLevelIndex < SceneManager.sceneCountInBuildSettings)
+		{
+			SceneManager.LoadScene(nextLevelIndex);
+		}
+		else
+		{
+			LoadLevel("Win Screen");
+		}
+	}
+}
