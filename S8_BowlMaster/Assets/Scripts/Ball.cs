3d2219fd96339040ed42ca219616b217e25ca0d1
Guillaume Grillon Labelle <iarus000@gmail.com>
2018-01-04 21:26:23 -0500

    S8_BowlMaster: L191: Add a bowling ball.

---
+++ S8_BowlMaster/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(AudioSource))]
+public class Ball : MonoBehaviour {
+    public float launchVelocity = 200f;
+
+    private Rigidbody rigibBody;
+    private AudioSource audioSource;
+
+    void Start ()
+    {
+        rigibBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        LaunchBall();
+    }
+
+    public void LaunchBall()
+    {
+        rigibBody.velocity = new Vector3(0, 0, launchVelocity);
+        audioSource.Play();
+    }
+}
